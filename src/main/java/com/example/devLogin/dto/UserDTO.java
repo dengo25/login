fe7b38f7b0bdd8ec13f5314ab8ce15f841268a9b
dengo25 <dengo25.dev@gmail.com>
2025-06-12 20:29:44 +0900

    [Feat] ResponseDTO 추가

--- src/main/java/com/example/devLogin/dto/UserDTO.java
+++ src/main/java/com/example/devLogin/dto/UserDTO.java
@@ -13,7 +13,11 @@ public class UserDTO {
   
   private String token; //인증 토큰 등 로그인 후 클라이언트에 전달됨
   private String username; //사용자명이나 이메일
-  private String password;
+  private String password; //응답에 사용시 jsonignore등으로 처리해야
   private Long id;
   
+  /*
+  * 1. 로그인 요청시
+  * 2. 로그인 응답 시에 사용
+  * */
 }
